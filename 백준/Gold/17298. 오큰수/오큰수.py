5bd74893f751d8e2ce0be1e890d929707dea3c72
Joowan Park <81845004+parkjoowan97@users.noreply.github.com>
2024-08-04 19:51:32 +0900

    [Gold IV] Title: 오큰수, Time: 1004 ms, Memory: 155536 KB -BaekjoonHub

---
+++ 백준/Gold/17298. 오큰수/오큰수.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+N = int(input())
+A = list(map(int, input().split()))
+answer = [-1] * N
+stack = [0] 
+for i in range(1, N):
+    while stack and A[stack[-1]] < A[i]:
+        answer[stack.pop()] = A[i]
+    stack.append(i)
+print(*answer)
