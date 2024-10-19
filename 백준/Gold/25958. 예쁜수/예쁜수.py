a0a4d79e3f33edcec4ea0385e9df3e92926977d9
Joowan Park <81845004+parkjoowan97@users.noreply.github.com>
2024-10-19 17:59:54 +0900

    [Gold IV] Title: 예쁜수, Time: 456 ms, Memory: 33240 KB -BaekjoonHub

--- 백준/Gold/25958. 예쁜수/예쁜수.py
+++ 백준/Gold/25958. 예쁜수/예쁜수.py
@@ -1,6 +1,5 @@
 import math
 N, K = map(int,input().split())
-#K는 소수인 것에 주의하면 될 것 같다
 
 pretty_number = []
 for i in range (1, N + 1):
@@ -12,8 +11,6 @@ for i in range (1, N + 1):
     if i % summary == 0:
         pretty_number.append(i)
 
-#중복 제거
-pretty_number = list(set(pretty_number))
 dp = [0 for _ in range (0, N + 1)]
 
 for i in range (0, len(pretty_number)):
