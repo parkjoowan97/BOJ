7029a7c817646a590b46170b24b1152e6366d66f
Joowan Park <81845004+parkjoowan97@users.noreply.github.com>
2024-10-29 15:57:41 +0900

    Delete 프로그래머스 directory

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.py
+++
@@ -1,9 +0,0 @@
-def solution(s):
-    number = list(s.split(" "))
-    new_num = []
-    for num in number:
-        new_num.append(int(num))
-    maximum,minimum = str(max(new_num)), str(min(new_num))
-    answer = minimum + " " + maximum
-     
-    return answer
