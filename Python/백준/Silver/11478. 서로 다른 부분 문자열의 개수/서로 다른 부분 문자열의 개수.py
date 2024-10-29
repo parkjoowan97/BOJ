8040c878acee1d527d9aa8c092b5080f4ac90854
Joowan Park <81845004+parkjoowan97@users.noreply.github.com>
2024-10-29 15:57:29 +0900

    Delete Python/백준/Silver/11478. 서로 다른 부분 문자열의 개수 directory

--- Python/백준/Silver/11478. 서로 다른 부분 문자열의 개수/서로 다른 부분 문자열의 개수.py
+++
@@ -1,8 +0,0 @@
-s = input()
-n = len(s)
-words = set()
-for i in range (1,n+1): #length
-    for k in range (0,n-i+1): #startpoint
-        words.add(s[k:k+i])
-words = list(words)
-print(len(words))
